78c406920e0c723bd377f077efc00682a8c69ecf
cazantyl <cazantyl@gmail.com>
2012-11-06 22:15:32 -0500

    Cleaned up imports

--- TheWalkingDead/src/com/orqwith/mc/TheWalkingDeadCommandExecutor.java
+++ TheWalkingDead/src/com/orqwith/mc/TheWalkingDeadCommandExecutor.java
@@ -1,14 +1,10 @@
 package com.orqwith.mc;
 
-import org.bukkit.Location;
 import org.bukkit.Server;
-import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Zombie;
-import org.bukkit.util.Vector;
 
 public class TheWalkingDeadCommandExecutor implements CommandExecutor {
 	private Server server;
