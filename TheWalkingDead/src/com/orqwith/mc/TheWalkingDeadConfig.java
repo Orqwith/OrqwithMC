2e3e21a2ae2187503011c470178a042fddc8d940
JavaWarlord <JavaWarlord@gmail.com>
2012-11-07 14:33:07 -0500

    Made variables private

--- TheWalkingDead/src/com/orqwith/mc/TheWalkingDeadConfig.java
+++ TheWalkingDead/src/com/orqwith/mc/TheWalkingDeadConfig.java
@@ -3,49 +3,59 @@ package com.orqwith.mc;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TheWalkingDeadConfig {
-	boolean immuneToDaylight = false;
-	int hordeSize = 5;
-	int spawnChance = 50;
-	int blockBreakSpawnInterval = 15;
-	int spawnInterval = 0;
-	
-	TheWalkingDeadConfig(JavaPlugin plugin)
-	{
+	private boolean immuneToDaylight = false;
+	private int hordeSize = 5;
+	private int spawnChance = 50;
+	private int blockBreakSpawnInterval = 15;
+	private int spawnInterval = 0;
+
+	TheWalkingDeadConfig(JavaPlugin plugin) {
 		this.hordeSize = plugin.getConfig().getInt("zombies.hordeSize");
 		this.spawnChance = plugin.getConfig().getInt("zombies.spawnChance");
-		this.blockBreakSpawnInterval = plugin.getConfig().getInt("zombies.blockBreakSpawnInterval");
-		this.immuneToDaylight = plugin.getConfig().getBoolean("zombies.immuneToDaylight");
+		this.blockBreakSpawnInterval = plugin.getConfig().getInt(
+				"zombies.blockBreakSpawnInterval");
+		this.immuneToDaylight = plugin.getConfig().getBoolean(
+				"zombies.immuneToDaylight");
 		this.spawnInterval = plugin.getConfig().getInt("zombies.spawnInterval") * 20;
 	}
-	
+
 	public boolean isImmuneToDaylight() {
 		return immuneToDaylight;
 	}
+
 	public void setImmuneToDaylight(boolean immuneToDaylight) {
 		this.immuneToDaylight = immuneToDaylight;
 	}
+
 	public int getHordeSize() {
 		return hordeSize;
 	}
+
 	public void setHordeSize(int hordeSize) {
 		this.hordeSize = hordeSize;
 	}
+
 	public int getSpawnChance() {
 		return spawnChance;
 	}
+
 	public void setSpawnChance(int spawnChance) {
 		this.spawnChance = spawnChance;
 	}
+
 	public int getBlockBreakSpawnInterval() {
 		return blockBreakSpawnInterval;
 	}
+
 	public void setBlockBreakSpawnInterval(int blockBreakSpawnInterval) {
 		this.blockBreakSpawnInterval = blockBreakSpawnInterval;
 	}
+
 	public int getSpawnInterval() {
 		return spawnInterval;
 	}
+
 	public void setSpawnInterval(int spawnInterval) {
 		this.spawnInterval = spawnInterval;
-	}	
+	}
 }
